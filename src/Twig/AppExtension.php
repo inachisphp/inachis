c60ad545f540a1bf729bd6679cf32b0d9df89683
David G. Paul <jedi58@gmail.com>
2025-06-26 10:53:08 +0100

    Added filesize unit conversion helper

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -30,4 +30,16 @@ class AppExtension extends AbstractExtension
     {
         return !empty($menuOption) && $menuOption == $selectedOption ? 'menu__active' : '';
     }
+
+    /**
+     * @param int $bytes
+     * @return string
+     */
+    public function bytesToMinimumUnit(int $bytes): string
+    {
+        $symbols = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+        $exp = (int) floor(log($bytes)/log(1024));
+
+        return sprintf('%.2f '. $symbols[$exp], ($bytes/pow(1024, floor($exp))));
+    }
 }
