2ea991de11c2928aa9addc6539aa5d32923d2c49
David G. Paul <jedi58@gmail.com>
2025-08-23 17:38:11 +0100

    Updated gulp tasks

--- bin/gulp/tasks/jscombinemin.js
+++ bin/gulp/tasks/jscombinemin.js
@@ -5,7 +5,6 @@ const config = require('../config');
 
 const concat = require('gulp-concat');
 const minify = require('gulp-babel-minify');
-const pump = require('pump');
 const rename = require('gulp-rename');
 
 const jsCompileAdmin = () => {
@@ -23,27 +22,23 @@ exports.jsCompile = gulp.parallel(
     jsCompileWeb
 );
 
-// exports.jsWatch = function() {
-//     gulp.watch(config.paths.src.js.all + '**/*.js', [
-//         'js:compile-web',
-//         'js:compile-admin'
-//     ]);
-// }
+exports.jsWatch = function()
+{
+    gulp.watch(config.paths.src.js.all + '**/*.js', exports.jsCompile);
+}
 
 function jsCompile (src, dest, callback)
 {
-    pump(
-        [
-            gulp.src([ config.paths.src.js.shared + '**/*.js', src + '**/*.js' ]),
-            concat('scripts.js'),
-            minify({
-                mangle: {
-                    keepClassName: true
-                }
-            }),
-            rename({ suffix: '.min' }),
-            gulp.dest(dest)
-        ],
-        callback
-    );
+    return gulp.src([
+            config.paths.src.js.shared + '*.js',
+            src + '*.js'
+        ])
+        .pipe(concat('scripts.js'))
+        .pipe(minify({
+            mangle: {
+                keepClassName: true
+            }
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(dest))
 }
