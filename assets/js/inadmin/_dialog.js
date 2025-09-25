2cf62cc0bddcac56b73d84dce23b84adb784534e
David G. Paul <jedi58@gmail.com>
2025-09-25 20:37:08 +0100

    [#214] Reimplemented session timeout dialog - now includes countdown

--- assets/js/inadmin/_dialog.js
+++ assets/js/inadmin/_dialog.js
@@ -10,6 +10,7 @@ var InachisDialog = {
             }
         }
     ],
+    preloadContent: '<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>',
     templateName: '',
     title: '',
     view: '',
@@ -25,15 +26,17 @@ var InachisDialog = {
         if (dialogWidth < 380) {
             dialogWidth = 376;
         }
-        let $dialogLink = $(event.currentTarget);
-        this.title = $dialogLink.data('title'),
-            this.view = $dialogLink.data('view'),
-            this.templateName = $dialogLink.data('templateName'),
-            this.className = $dialogLink.data('className');//,
-            //this.buttons = JSON.parse(window.atob($dialogLink.data('buttons')));
+        if (null !== event) {
+            let $dialogLink = $(event.currentTarget);
+            this.title = $dialogLink.data('title'),
+                this.view = $dialogLink.data('view'),
+                this.templateName = $dialogLink.data('templateName'),
+                this.className = $dialogLink.data('className');//,
+            this.buttons = JSON.parse(window.atob($dialogLink.data('buttons')));
+        }
         this.buttons = (typeof this.buttons != 'undefined' && this.buttons instanceof Array) ? this.buttons : [ this.buttons ];
 
-        $('<div id="' + this.className + '"><form class="form"><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p></form></div>').dialog(
+        $('<div id="' + this.className + '"><form class="form">'+this.preloadContent+'</form></div>').dialog(
             {
                 buttons: [].concat(this.buttons),
                 close: function()
@@ -58,18 +61,25 @@ var InachisDialog = {
         );
     },
 
-    addDialogContent: function(templateName)
+    addDialogContent: function(templateName = '')
     {
         $('.ui-dialog-titlebar-close').addClass('material-icons').html('close');
-        $('.ui-dialog-content').load(
-            Inachis.prefix + '/ax/' + this.hyphenToCamel(templateName) + '/get', {
-                selectedImage: ''
-            }, function() // response, status, xhr
-            {
-                $(this).parent().removeClass('ui-dialog-loading');
-                $('.ui-dialog').position({my: 'center', at: 'center', of: window});
-            }
-        );
+        if (templateName !== '') {
+            $('.ui-dialog-content').load(
+                Inachis.prefix + '/ax/' + this.hyphenToCamel(templateName) + '/get', {
+                    selectedImage: ''
+                }, function() // response, status, xhr
+                {
+                    $(this).parent().removeClass('ui-dialog-loading');
+                    $('.ui-dialog').position({my: 'center', at: 'center', of: window});
+                }
+            );
+        } else {
+            $('.ui-dialog')
+                .removeClass('ui-dialog-loading')
+                .position({my: 'center', at: 'center', of: window})
+            ;
+        }
     },
 
     hyphenToCamel: function(hyphenatedString)
