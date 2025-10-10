13133308517891269cc03fe2a3d2155714368ad5
David G. Paul <dave.paul@jadu.net>
2025-10-10 15:39:35 +0100

    Used stricter types

--- src/Util/ReadingTime.php
+++ src/Util/ReadingTime.php
@@ -7,7 +7,8 @@ class ReadingTime
     private const WORDS_PER_MINUTE = 238;
 
     /**
-     * @param string $text
+     * @param string|null $text
+     * @param int|null $wordCount
      * @param int|null $wpm
      * @return int
      */
@@ -17,17 +18,17 @@ class ReadingTime
     }
 
     /**
-     * @param string $text
+     * @param string|null $text
      * @return int
      */
     public static function getWordCount(?string $text): int
     {
         // @todo: consider removing markdown from string also
-        return str_word_count(strip_tags($text));
+        return str_word_count(strip_tags((string) $text));
     }
 
     /**
-     * @param string $text
+     * @param string|null $text
      * @param int|null $wpm
      * @return array
      */
