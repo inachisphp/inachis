ed1c44ae16f838795d549309550a059cecb65d10
David G. Paul <dave.paul@jadu.net>
2025-10-16 15:29:44 +0100

    Split app:localise-images command up into services for easy re-use

---
+++ tests/phpunit/Service/Image/ContentImageUpdaterTest.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is part of the inachis framework
+ * 
+ * @package Inachis
+ * @license https://github.com/inachisphp/inachis/blob/main/LICENSE.md
+ */
+
+namespace App\Tests\phpunit\Service\Image;
+
+use App\Entity\Page;
+use App\Entity\Revision;
+use App\Repository\RevisionRepository;
+use App\Service\Image\ContentImageUpdater;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+
+class DummyEntity
+{
+    private string $content = 'Original content with https://example.com/image.jpg';
+    private DateTime $modDate;
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+    public function setContent(string $value): void
+    {
+        $this->content = $value;
+    }
+    public function setModDate(DateTime $date): void
+    {
+        $this->modDate = $date;
+    }
+}
+
+class ContentImageUpdaterTest extends TestCase
+{
+    private EntityManagerInterface $em;
+    private ContentImageUpdater $updater;
+
+    protected function setUp(): void
+    {
+        $this->em = $this->createMock(EntityManagerInterface::class);
+        $this->updater = new ContentImageUpdater($this->em);
+    }
+
+    public function testUpdatesEntityContentAndPersists(): void
+    {
+        $page = new Page('test', 'https://example.com/image.jpg');
+        $revision = $this->createMock(Revision::class);
+        $revision->expects($this->once())
+            ->method('setAction')
+            ->with(RevisionRepository::UPDATED);
+        $revisionRepository = $this->createMock(RevisionRepository::class);
+        $revisionRepository->expects($this->once())
+            ->method('hydrateNewRevisionFromPage')
+            ->with($page)
+            ->willReturn($revision);
+        $this->em->method('getRepository')
+            ->with(Revision::class)
+            ->willReturn($revisionRepository);
+
+        $this->em->expects($this->exactly(2))
+            ->method('persist')
+            ->willReturnCallback(function ($arg) use ($revision, $page) {
+                static $call = 0;
+                $call++;
+                if ($call === 1) {
+                    $this->assertSame($revision, $arg, 'Added Revision');
+                } elseif ($call === 2) {
+                    $this->assertSame($page, $arg, 'Updated entity');
+                }
+            });
+        $this->em->expects($this->once())->method('flush');
+
+        $this->updater->updateEntity(
+            $page,
+            'content',
+            [
+                'source' => ['https://example.com/image.jpg'],
+                'destination' => ['/imgs/image.jpg']
+            ],
+            true
+        );
+
+        $this->assertStringContainsString('/imgs/image.jpg', $page->getContent());
+    }
+}
---
+++ tests/phpunit/Service/Image/ImageLocaliserTest.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * This file is part of the inachis framework
+ * 
+ * @package Inachis
+ * @license https://github.com/inachisphp/inachis/blob/main/LICENSE.md
+ */
+
+namespace App\Tests\phpunit\Service\Image;
+
+use App\Service\Image\ImageLocaliser;
+use PHPUnit\Framework\MockObject\MockClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Exception;
+
+class ImageLocaliserMockedTest extends TestCase
+{
+    private $filesystem;
+    private $localiser;
+
+    protected function setUp(): void
+    {
+        $this->filesystem = $this->createMock(Filesystem::class);
+        $this->localiser = new ImageLocaliser($this->filesystem, '/fake/public/imgs');
+    }
+
+    public function testCreatesDirectoryIfNotExists(): void
+    {
+        $filesystem = $this->createMock(Filesystem::class);
+        $filesystem->expects($this->once())->method('exists')->with('/fake/path')->willReturn(false);
+        $filesystem->expects($this->once())->method('mkdir')->with('/fake/path', 0777);
+
+        new ImageLocaliser($filesystem, '/fake/path');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testDownloadExceptionIfStreamFails(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessageMatches('/Failed to move file/');
+        $result = $this->localiser->downloadToLocal('invalid://url');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testDownloadExceptionIfFileEmpty(): void
+    {
+        if (!function_exists('mime_content_type')) {
+            $this->markTestSkipped('mime_content_type not available');
+        }
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessageMatches('/Failed to move file/');
+        $tmpFile = sys_get_temp_dir() . '/test/testfile2.txt';
+        $this->prepareTestFile($tmpFile, '');
+        $this->localiser->downloadToLocal('file://' . $tmpFile);
+        @unlink($tmpFile);
+    }
+
+
+    /**
+     * @throws Exception
+     */
+    public function testAddsExtensionWhenMissing(): void
+    {
+        if (!function_exists('mime_content_type')) {
+            $this->markTestSkipped('mime_content_type not available');
+        }
+        $tmpFile = sys_get_temp_dir() . '/test/testfile_noext';
+        $this->prepareTestFile($tmpFile, 'abc123');
+        $renameCalls = [];
+        $this->filesystem->method('rename')
+            ->willReturnCallback(function ($from, $to) use (&$renameCalls) {
+                $renameCalls[] = [$from, $to];
+                return true;
+            });
+        $this->assertEquals('/imgs/testfile_noext.plain', $this->localiser->downloadToLocal('file://' . $tmpFile));
+        @unlink($tmpFile);
+    }
+
+    public function testThrowsExceptionIfRenameFails(): void
+    {
+        if (!function_exists('mime_content_type')) {
+            $this->markTestSkipped('mime_content_type not available');
+        }
+        $tmpFile = sys_get_temp_dir() . '/test/testfile2.txt';
+        $this->prepareTestFile($tmpFile, 'abc123');
+        $this->filesystem
+            ->expects($this->once())
+            ->method('rename')
+            ->willThrowException(new Exception('rename failed'));
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessageMatches('/Failed to move file/');
+        $this->localiser->downloadToLocal('file://' . $tmpFile);
+        @unlink($tmpFile);
+    }
+
+    private function prepareTestFile(string $filename, string $contents = ''): void
+    {
+        if (!is_dir(sys_get_temp_dir() . '/test')) {
+            mkdir(sys_get_temp_dir() . '/test');
+        }
+        file_put_contents($filename, $contents);
+    }
+
+    protected function tearDown(): void
+    {
+        @rmdir(sys_get_temp_dir() . '/test');
+        @unlink(sys_get_temp_dir() . '/test/testfile_noext');
+        @unlink(sys_get_temp_dir() . '/test/testfile2.txt');
+    }
+}
