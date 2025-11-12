37f8c55cf84dafe57bbff33bd0b50a2987d7c232
David G. Paul <dave.paul@jadu.net>
2025-11-12 18:31:24 +0000

    Tidied up SearchRepository to avoid repetition

--- src/Repository/SearchRepository.php
+++ src/Repository/SearchRepository.php
@@ -32,33 +32,23 @@ class SearchRepository extends AbstractRepository
                            int     $limit = 25,
                            string  $orderBy = 'relevance DESC, contentDate DESC'
     ): SearchResult {
-        $sql = "
-            (
-                SELECT
-                    s.id, s.title, s.sub_title, s.description AS content, 'Series' AS type, s.last_date AS contentDate, s.mod_date,
-                        (
-                            (CASE WHEN LOWER(s.title) LIKE :kw THEN 10 ELSE 0 END) +
-                            (CASE WHEN LOWER(s.sub_title) LIKE :kw THEN 5 ELSE 0 END) +
-                            (CASE WHEN LOWER(s.description) LIKE :kw THEN 2 ELSE 0 END)
-                        ) AS relevance
-                FROM series s
-                WHERE LOWER(s.title) LIKE :kw OR LOWER(s.sub_title) LIKE :kw OR LOWER(s.description) LIKE :kw
-            )
-            UNION ALL
-            (
-                SELECT
-                    p.id, p.title, p.sub_title, p.content, CONCAT(UCASE(LEFT(type, 1)), LCASE(SUBSTRING(type, 2))) AS type, p.post_date AS contentDate, p.mod_date,
-                    (
-                        (CASE WHEN LOWER(p.title) LIKE :kw THEN 10 ELSE 0 END) +
-                        (CASE WHEN LOWER(p.sub_title) LIKE :kw THEN 5 ELSE 0 END) +
-                        (CASE WHEN LOWER(p.content) LIKE :kw THEN 2 ELSE 0 END)
-                    ) AS relevance
-                FROM page p
-                WHERE LOWER(p.title) LIKE :kw OR LOWER(p.sub_title) LIKE :kw OR LOWER(p.content) LIKE :kw
-            )
-            ORDER BY $orderBy 
-            LIMIT :limit OFFSET :offset
-        ";
+        $sql = sprintf('%s ORDER BY %s LIMIT :limit OFFSET :offset;',
+            $this->getSQLUnion([
+                'p.id, p.title, p.sub_title, p.content, CONCAT(UCASE(LEFT(type, 1)), LCASE(SUBSTRING(type, 2))) AS type, p.post_date AS contentDate, p.mod_date,
+                (
+                    (CASE WHEN LOWER(p.title) LIKE :kw THEN 10 ELSE 0 END) +
+                    (CASE WHEN LOWER(p.sub_title) LIKE :kw THEN 5 ELSE 0 END) +
+                    (CASE WHEN LOWER(p.content) LIKE :kw THEN 2 ELSE 0 END)
+                ) AS relevance',
+                's.id, s.title, s.sub_title, s.description AS content, \'Series\' AS type, s.last_date AS contentDate, s.mod_date,
+                (
+                    (CASE WHEN LOWER(s.title) LIKE :kw THEN 10 ELSE 0 END) +
+                    (CASE WHEN LOWER(s.sub_title) LIKE :kw THEN 5 ELSE 0 END) +
+                    (CASE WHEN LOWER(s.description) LIKE :kw THEN 2 ELSE 0 END)
+                ) AS relevance',
+            ]),
+            $orderBy,
+        );
 
         $stmt = $this->connection->prepare($sql);
         $stmt->bindValue('kw', '%' . strtolower($keyword) . '%');
@@ -76,19 +66,32 @@ class SearchRepository extends AbstractRepository
      */
     private function getSearchTotalResults($keyword): int
     {
-        $sql = "
-            SELECT COUNT(*) AS total FROM (
-                SELECT id FROM series
-                WHERE
-                    LOWER(title) LIKE :kw OR LOWER(sub_title) LIKE :kw OR LOWER(description) LIKE :kw
-                UNION ALL
-                SELECT id FROM page
-                WHERE
-                    LOWER(title) LIKE :kw OR LOWER(sub_title) LIKE :kw OR LOWER(content) LIKE :kw
-            ) AS all_results
-        ";
+        $sql = sprintf('SELECT COUNT(*) AS total FROM (%s) AS all_results;',
+            $this->getSQLUnion([ 'id', 'id' ])
+        );
         return $this->connection->prepare($sql)
             ->executeQuery([ 'kw' => '%' . $keyword . '%' ])
             ->fetchOne();
+    }
+
+    private function getSQLUnion($fieldLists)
+    {
+        return sprintf('
+            (SELECT %s FROM page p WHERE %s)
+            UNION ALL
+            (SELECT %s FROM series s WHERE %s)',
+            $fieldLists[0],
+            $this->getWhereConditions('page'),
+            $fieldLists[1],
+            $this->getWhereConditions('series'),
+        );
+    }
+
+    private function getWhereConditions($type)
+    {
+        return match($type) {
+            'page' => 'LOWER(p.title) LIKE :kw OR LOWER(p.sub_title) LIKE :kw OR LOWER(p.content) LIKE :kw',
+            'series' => 'LOWER(s.title) LIKE :kw OR LOWER(s.sub_title) LIKE :kw OR LOWER(s.description) LIKE :kw',
+        };
     }
 }
