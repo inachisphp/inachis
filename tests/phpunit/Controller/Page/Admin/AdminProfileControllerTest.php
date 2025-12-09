c743fab2b729530e8cb8a43f94baa31bf69c7180
David G. Paul <dave.paul@jadu.net>
2025-12-09 11:49:06 +0000

    Updated phpunit tests

--- tests/phpunit/Controller/Page/Admin/AdminProfileControllerTest.php
+++ tests/phpunit/Controller/Page/Admin/AdminProfileControllerTest.php
@@ -10,17 +10,35 @@
 namespace App\Tests\phpunit\Controller\Page\Admin;
 
 use App\Controller\Page\Admin\AdminProfileController;
+use App\Entity\User;
+use App\Form\UserType;
 use App\Model\ContentQueryParameters;
-use App\Repository\CategoryRepository;
 use App\Repository\UserRepository;
+use App\Service\User\UserBulkActionService;
+use App\Service\User\PasswordResetTokenService;
+use App\Service\User\UserRegistrationService;
+use App\Transformer\ImageTransformer;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
 use PHPUnit\Framework\MockObject\Exception;
+use Ramsey\Uuid\Uuid;
+use Random\RandomException;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\Button;
+use Symfony\Component\Form\ClickableInterface;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\PreloadedExtension;
+use Symfony\Component\Form\SubmitButton;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Validator\Validation;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AdminProfileControllerTest extends WebTestCase
@@ -37,7 +55,14 @@ class AdminProfileControllerTest extends WebTestCase
         $translator = $this->createMock(TranslatorInterface::class);
         $this->controller = $this->getMockBuilder(AdminProfileController::class)
             ->setConstructorArgs([$entityManager, $security, $translator])
-            ->onlyMethods(['createFormBuilder', 'render'])
+            ->onlyMethods([
+                'addFlash',
+                'createForm',
+                'createFormBuilder',
+                'generateUrl',
+                'redirect',
+                'render',
+            ])
             ->getMock();
         $this->controller->method('render')
             ->willReturnCallback(function (string $template, array $data) {
@@ -48,6 +73,10 @@ class AdminProfileControllerTest extends WebTestCase
 
         parent::setUp();
     }
+
+    /**
+     * @throws Exception
+     */
     public function testList(): void
     {
         $request = new Request([], [], [
@@ -56,6 +85,7 @@ class AdminProfileControllerTest extends WebTestCase
         ], [], [], [
             'REQUEST_URI' => '/incc/admin/list/50/25'
         ]);
+        $userBulkActionService = $this->createMock(UserBulkActionService::class);
         $userRepository = $this->createMock(UserRepository::class);
         $contentQueryParameters = $this->createMock(ContentQueryParameters::class);
         $contentQueryParameters->method('process')->willReturn([
@@ -63,7 +93,115 @@ class AdminProfileControllerTest extends WebTestCase
             'offset' => 50,
             'limit' => 25,
         ]);
-        $result = $this->controller->list($request, $userRepository, $contentQueryParameters);
+        $result = $this->controller->list($request, $contentQueryParameters, $userBulkActionService, $userRepository);
         $this->assertEquals('rendered:inadmin/page/admin/list.html.twig', $result->getContent());
+    }
+
+
+
+    /**
+     * @throws Exception
+     */
+    public function testListDeleteAction(): void
+    {
+        $request = new Request([], [
+            'delete' => '',
+            'items' => [
+                Uuid::uuid1()->toString(),
+            ],
+        ], [
+            'offset' => 50,
+            'limit' => 25,
+        ], [], [], [
+            'REQUEST_URI' => '/incc/admin/list/50/25'
+        ]);
+        $userBulkActionService = $this->createMock(UserBulkActionService::class);
+        $userRepository = $this->createMock(UserRepository::class);
+        $contentQueryParameters = $this->createMock(ContentQueryParameters::class);
+        $contentQueryParameters->method('process')->willReturn([
+            'filters' => [],
+            'offset' => 50,
+            'limit' => 25,
+        ]);
+        $form = $this->createMock(Form::class);
+        $form->method('isSubmitted')->willReturn(true);
+        $this->controller->method('createForm')->willReturn($form);
+
+        $result = $this->controller->list($request, $contentQueryParameters, $userBulkActionService, $userRepository);
+        $this->assertEquals('rendered:inadmin/page/admin/list.html.twig', $result->getContent());
+    }
+
+    /**
+     * @throws Exception
+     * @throws RandomException
+     */
+    public function testEditView(): void
+    {
+        $request = new Request([], [], [
+            'id' => 'test-user',
+        ], [], [], [
+            'REQUEST_URI' => '/incc/admin/test-user'
+        ]);
+        $imageTransformer = $this->createMock(ImageTransformer::class);
+        $userRegistrationService = $this->createMock(UserRegistrationService::class);
+        $user = new User();
+        $userRepository = $this->createMock(UserRepository::class);
+        $userRepository->method('findOneBy')->willReturn($user);
+
+        $form = $this->createMock(Form::class);
+        $this->controller->method('createForm')->willReturn($form);
+
+        $result = $this->controller->edit(
+            $request,
+            $imageTransformer,
+            $userRegistrationService,
+            $userRepository
+        );
+        $this->assertEquals('rendered:inadmin/page/admin/profile.html.twig', $result->getContent());
+    }
+
+    /**
+     * @throws Exception
+     * @throws RandomException
+     */
+    public function testEditSave(): void
+    {
+        $formData = [
+            'user' => [
+                'username' => 'test-user',
+                'displayName' => 'Test user',
+                'email' => 'test-user@example.com',
+                'timezone' => 'UTC',
+            ],
+        ];
+        $request = new Request([], [ $formData, ], [
+            'id' => 'new',
+        ], [], [], [
+            'REQUEST_URI' => '/incc/admin/test-user'
+        ]);
+        $request->setMethod(Request::METHOD_POST);
+
+        $imageTransformer = $this->createMock(ImageTransformer::class);
+        $userRegistrationService = $this->createMock(UserRegistrationService::class);
+        $user = (new User())->setEmail('test-user@example.com');
+        $userRepository = $this->createMock(UserRepository::class);
+        $userRepository->method('findOneBy')->willReturn($user);
+
+        $form = $this->createMock(Form::class);
+        $form->method('isSubmitted')->willReturn(true);
+        $form->method('isValid')->willReturn(true);
+        $form->method('has')->willReturnMap([
+            [ 'enableDisable', false ],
+            [ 'delete', false ],
+        ]);
+        $this->controller->method('createForm')->willReturn($form);
+
+        $result = $this->controller->edit(
+            $request,
+            $imageTransformer,
+            $userRegistrationService,
+            $userRepository
+        );
+        $this->assertInstanceOf(RedirectResponse::class, $result);
     }
 }
