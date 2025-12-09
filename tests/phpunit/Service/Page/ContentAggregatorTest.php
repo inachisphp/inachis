45032a1dc3d26c506b35a38634a0a0353e4ed41b
David G. Paul <dave.paul@jadu.net>
2025-12-09 10:52:29 +0000

    Corrected test namespace

--- tests/phpunit/Service/Page/ContentAggregatorTest.php
+++ tests/phpunit/Service/Page/ContentAggregatorTest.php
@@ -7,7 +7,7 @@
  * @license https://github.com/inachisphp/inachis/blob/main/LICENSE.md
  */
 
-namespace App\Tests\Service\Page;
+namespace App\Tests\phpunit\Service\Page;
 
 use App\Entity\Page;
 use App\Entity\Series;
