a6f9bd3f805fbd0dfcb9fad548a4755ad408ebbe
David G. Paul <jedi58@gmail.com>
2026-01-10 12:39:09 +0000

    Added page for viewing email DNS settings

---
+++ src/Controller/Page/Setting/EmailSettingController.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the inachis framework
+ *
+ * @package Inachis
+ * @license https://github.com/inachisphp/inachis/blob/main/LICENSE.md
+ */
+
+namespace App\Controller\Page\Setting;
+
+use App\Controller\AbstractInachisController;
+use App\Service\System\Domain\DNSFetcherService;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+class EmailSettingController extends AbstractInachisController
+{
+    #[Route("/incc/settings/email")]
+    public function index(
+		Request $request,
+		DNSFetcherService $dnsFetcherService,
+	): Response {
+        $domain = $_ENV['APP_DOMAIN'] ?? 'example.com';
+
+		$this->data['domain'] = $domain;
+		$this->data['mx_records'] = $dnsFetcherService->fetchMXRecords($domain);
+        $this->data['dmarc'] = $dnsFetcherService->fetchDMARCRecords($domain);
+		$this->data['spf'] = $dnsFetcherService->fetchSPFRecords($domain);
+		$this->data['dmarc_errors'] = $dnsFetcherService->validateDmarc();
+		$this->data['spf_errors'] = $dnsFetcherService->validateSPFRecord();
+
+		return $this->render('inadmin/page/setting/email.html.twig', $this->data);
+	}
+}
