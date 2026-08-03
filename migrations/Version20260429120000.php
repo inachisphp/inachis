6b6199c99c0bdd9c21a881372dfac8028051a06e
David G. Paul <jedi58@users.noreply.github.com>
2026-08-03 20:20:19 +0100

    Add namespace and use statements to migration file

--- migrations/Version20260429120000.php
+++ migrations/Version20260429120000.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
 final class Version20260429120000 extends AbstractMigration
 {
     public function getDescription(): string
